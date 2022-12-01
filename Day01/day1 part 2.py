bbe0ee92f7b22fe05f6309658c644c7aad7845e6
David Betteridge <davidabetteridge@gmail.com>
2022-12-01 08:55:22 +0000

    Using heapq.heapify

--- Day01/day1 part 2.py
+++ Day01/day1 part 2.py
@@ -1,17 +1,18 @@
-from collections import defaultdict
-
+import heapq
 
 with open("Day01/data.txt") as f:
   lines = [line.strip("\n") for line in f.readlines()]
 
 elf_number = 0
-totals = defaultdict(int)
+totals = [0]
 
 for line_number, line in enumerate(lines):
   if line == "":
     elf_number += 1
+    totals.append(0)
   else:
     totals[elf_number] += int(line)
 
-calories = sorted(totals.values(), reverse=True)
-print(sum(calories[:3]))
+heapq.heapify(totals)
+print("Part1", sum(heapq.nlargest(1, totals)))
+print("Part2", sum(heapq.nlargest(3, totals)))
