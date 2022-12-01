1138c20eab90d5539942e6e32cb53363f5a16fcc
David Betteridge <davidabetteridge@gmail.com>
2022-12-01 08:15:57 +0000

    Day 1 Part 1

---
+++ Day01/day1.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+
+with open("Day01/data.txt") as f:
+  lines = f.readlines()
+
+elf_number = 0
+totals = defaultdict(int)
+
+for line_number, line in enumerate(lines):
+  if line == "\n":
+    elf_number += 1
+  else:
+    totals[elf_number] += int(line.strip("\n"))
+
+print(max(totals.values()))
