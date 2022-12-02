36159ce228a321730043576c0e37aa78dec59138
David Betteridge <davidabetteridge@gmail.com>
2022-12-02 08:35:04 +0000

    smaller day 1

--- Day01/day1 part 2.py
+++ Day01/day1 part 2.py
@@ -1,18 +1,7 @@
 import heapq
-
 with open("Day01/data.txt") as f:
-  lines = [line.strip("\n") for line in f.readlines()]
-
-elf_number = 0
-totals = [0]
-
-for line_number, line in enumerate(lines):
-  if line == "":
-    elf_number += 1
-    totals.append(0)
-  else:
-    totals[elf_number] += int(line)
-
+  totals = [ sum([int(line) for line in block.split("\n")]) for block in f.read().split("\n\n")]
 heapq.heapify(totals)
 print("Part1", sum(heapq.nlargest(1, totals)))
 print("Part2", sum(heapq.nlargest(3, totals)))
+
