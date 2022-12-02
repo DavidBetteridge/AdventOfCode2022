5138e884e55201b591ed9641ad1aeac42e50befd
David Betteridge <davidabetteridge@gmail.com>
2022-12-02 08:58:31 +0000

    Refactor day 2

--- Day02/day2.py
+++ Day02/day2.py
@@ -1,19 +1,32 @@
-scores = {
-  "A X": 3 + 1,
-  "A Y": 6 + 2,
-  "A Z": 0 + 3,
-  
-  "B X": 0 + 1,
-  "B Y": 3 + 2,
-  "B Z": 6 + 3,
+WIN = 6; DRAW = 3; LOSE = 0
+ROCK = 1; PAPER = 2; SCISSORS = 3
 
-  "C X": 6 + 1,
-  "C Y": 0 + 2,
-  "C Z": 3 + 3,
+part1_scores = {
+  "A X": DRAW + ROCK,
+  "A Y": WIN + PAPER,
+  "A Z": LOSE + SCISSORS,
+  "B X": LOSE + ROCK,
+  "B Y": DRAW + PAPER,
+  "B Z": WIN + SCISSORS,
+  "C X": WIN + ROCK,
+  "C Y": LOSE + PAPER,
+  "C Z": DRAW + SCISSORS,
+}
+
+part2_scores = {
+  "A X": LOSE + SCISSORS,
+  "A Y": DRAW + ROCK,
+  "A Z": WIN + PAPER,
+  "B X": LOSE + ROCK,
+  "B Y": DRAW + PAPER,
+  "B Z": WIN + SCISSORS,
+  "C X": LOSE + PAPER,
+  "C Y": DRAW + SCISSORS,
+  "C Z": WIN + ROCK,
 }
 
 with open("Day02/data.txt") as f:
   rounds = f.read().splitlines()
 
-total = sum(scores[round] for round in rounds)
-print(total)
+print("Part1", sum(part1_scores[round] for round in rounds))
+print("Part2", sum(part2_scores[round] for round in rounds))
