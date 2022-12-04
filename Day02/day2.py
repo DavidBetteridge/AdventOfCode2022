23a031ee9d8b9713ae2c9095f749c6f2a3559247
David Betteridge <davidabetteridge@gmail.com>
2022-12-04 10:02:58 +0000

    Short version of day 2

--- Day02/day2.py
+++ Day02/day2.py
@@ -1,32 +1,18 @@
 WIN = 6; DRAW = 3; LOSE = 0
 ROCK = 1; PAPER = 2; SCISSORS = 3
 
-part1_scores = {
-  "A X": DRAW + ROCK,
-  "A Y": WIN + PAPER,
-  "A Z": LOSE + SCISSORS,
-  "B X": LOSE + ROCK,
-  "B Y": DRAW + PAPER,
-  "B Z": WIN + SCISSORS,
-  "C X": WIN + ROCK,
-  "C Y": LOSE + PAPER,
-  "C Z": DRAW + SCISSORS,
-}
-
-part2_scores = {
-  "A X": LOSE + SCISSORS,
-  "A Y": DRAW + ROCK,
-  "A Z": WIN + PAPER,
-  "B X": LOSE + ROCK,
-  "B Y": DRAW + PAPER,
-  "B Z": WIN + SCISSORS,
-  "C X": LOSE + PAPER,
-  "C Y": DRAW + SCISSORS,
-  "C Z": WIN + ROCK,
+scores = {
+  "A X": (DRAW + ROCK, LOSE + SCISSORS),
+  "A Y": (WIN + PAPER, DRAW + ROCK),
+  "A Z": (LOSE + SCISSORS, WIN + PAPER),
+  "B X": (LOSE + ROCK, LOSE + ROCK),
+  "B Y": (DRAW + PAPER, DRAW + PAPER),
+  "B Z": (WIN + SCISSORS, WIN + SCISSORS),
+  "C X": (WIN + ROCK, LOSE + PAPER),
+  "C Y": (LOSE + PAPER, DRAW + SCISSORS),
+  "C Z": (DRAW + SCISSORS, WIN + ROCK),
 }
 
 with open("Day02/data.txt") as f:
-  rounds = f.read().splitlines()
-
-print("Part1", sum(part1_scores[round] for round in rounds))
-print("Part2", sum(part2_scores[round] for round in rounds))
+  rows = [scores[round.strip()] for round in f]
+  print(*(sum(row[part] for row in rows) for part in (0, 1)))
