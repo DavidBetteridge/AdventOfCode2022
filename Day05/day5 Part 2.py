08d61b81ba57a9c546d99acacc26b97ff9912042
David Betteridge <davidabetteridge@gmail.com>
2022-12-05 09:11:12 +0000

    Day 5 - Part 2

---
+++ Day05/day5 Part 2.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+
+
+with open("Day05/data.txt") as f:
+  lines = f.read().splitlines()
+
+  n = 0
+  stacks: defaultdict[int, list[str]] = defaultdict(list)
+  num_of_stacks = (len(lines[0])+1) // 4
+  while lines[n][1] != "1":
+    for s in range(num_of_stacks):
+      crate = lines[n][(s*4)+1]
+      if crate != " ":
+        stacks[s+1].insert(0, lines[n][(s*4)+1])
+    n+=1
+  n+=2  #Skip the blank line
+
+  for line in lines[n:]:
+    lhs, rhs = line.split(" from ")
+    qty = int(lhs.split(" ")[1])
+    src, dst = rhs.split(" to ")
+    cs = stacks[int(src)][-qty:]
+    stacks[int(src)] = stacks[int(src)][:-qty]
+    stacks[int(dst)] = stacks[int(dst)] + cs
+    
+  for stack in range(num_of_stacks):
+    print(stacks[stack+1][-1], end="")   # GTBQZHHVF
