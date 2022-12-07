311aca56ff9176833f9e0cd36968fc751593bbfd
David Betteridge <davidabetteridge@gmail.com>
2022-12-07 20:29:20 +0000

    Nice refactor

--- Day07/day7.py
+++ Day07/day7.py
@@ -1,30 +1,26 @@
 
+from collections import defaultdict
 from typing import Dict, List
 DIR_SEP = "\\"
 
 with open("Day07/data.txt") as f:
   path: List[str] = []
-  directories: Dict[str, int] = dict()
+  sizes: Dict[str, int] = defaultdict(int)
   for line in f.read().splitlines():
     match line.split():
       case "$", "cd", "..":
         path = path[:-1]
       case "$", "cd", sub_folder:
         path.append(sub_folder)
-        if DIR_SEP.join(path) not in directories:
-          directories[ DIR_SEP.join(path)] = 0
       case "$", "ls":
         pass
       case "dir", sub_folder:
         pass
       case text_size, filename:
-        directories[DIR_SEP.join(path)] += int(text_size)
+        for i in range(len(path)):
+          sizes[DIR_SEP.join(path[:i+1])] += int(text_size)
       case _:
-        raise Exception("Unknown expected")
-
-sizes = {directory: 
-          sum([directories[sub] for sub in directories if sub.startswith(directory)])
-         for directory in directories }
+        raise Exception("Should not happen")
 
 part1 = sum(size for size in sizes.values() if size <= 100000)
 print(part1)
