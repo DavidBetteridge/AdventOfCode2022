75c9d7e7c282f27fb31d2629ecf831c27760a114
David Betteridge <davidabetteridge@gmail.com>
2022-12-08 17:33:09 +0000

    Refactor part 2

--- Day08/day8 part 2.py
+++ Day08/day8 part 2.py
@@ -1,78 +1,28 @@
-from typing import Set, Tuple
-
-
-def visible_from_tree(column_number: int, row_number: int):
-  #Left to right
-  max_height = int(rows[row_number][column_number])
-  x = column_number + 1
-  visible_right = 0
-  while x < n_columns:
-    if int(rows[row_number][x]) >= max_height:
-      visible_right+=1
-      break
-    elif int(rows[row_number][x]) < max_height:
-      x += 1
-      visible_right+=1
-    else:
-      break
-
-  #Right to left
-  max_height = int(rows[row_number][column_number])
-  x = column_number - 1
-  visible_left = 0
-  while x >= 0:
-    if int(rows[row_number][x]) >= max_height:
-      visible_left+=1
-      break
-    elif int(rows[row_number][x]) < max_height:
-      x -= 1
-      visible_left+=1
-    else:
-      break
-
-  #Downwards
-  max_height = int(rows[row_number][column_number])
-  y = row_number + 1
-  visible_down = 0
-  while y < n_rows:
-    if int(rows[y][column_number]) >= max_height:
-      visible_down+=1
-      break
-    elif int(rows[y][column_number]) < max_height:
-      y += 1
-      visible_down+=1
-    else:
-      break
-
-  #Upwards
-  max_height = int(rows[row_number][column_number])
-  y = row_number - 1
-  visible_up = 0
-  while y >= 0:
-    if int(rows[y][column_number]) >= max_height:
-      visible_up+=1
-      break
-    elif int(rows[y][column_number]) < max_height:
-      y -= 1
-      visible_up+=1
-    else:
-      break
-
-  return visible_left*visible_right*visible_down*visible_up  
-
+from itertools import product
 
 with open("Day08/data.txt") as f:
-  rows = f.read().splitlines()
-  n_rows = len(rows)
-  n_columns = len(rows[0])
+  trees = f.read().splitlines()
+  n_rows = len(trees)
+  n_columns = len(trees[0])
 
-  seen: Set[Tuple[int,int]] = set()
-  best_score = 0
+def visible_from_tree(column_number: int, row_number: int):
+  score = 1
+  max_height = int(trees[row_number][column_number])
+  for x_offset, y_offset in [(1,0), (-1,0), (0,-1), (0,1)]:
+    x = column_number + x_offset
+    y = row_number + y_offset
+    num_visible = 0
+    view_blocked = False
+    while (0 <= x < n_columns) and (0 <= y < n_rows) and not view_blocked:
+      num_visible+=1
+      if int(trees[y][x]) >= max_height:
+        view_blocked = True
+      x += x_offset
+      y += y_offset
+    score *= num_visible
+  return score
 
-  for row_number, row in enumerate(rows):
-
-    for column_number in range(n_columns):
-      score = visible_from_tree(column_number, row_number)
-      best_score = max(best_score, score)
-
-print(best_score)      #671160
+part2_score = max([visible_from_tree(column_number, row_number)
+                    for row_number, column_number
+                    in product(range(n_rows), range(n_columns))])
+assert part2_score == 671160
