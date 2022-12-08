0ee842558e56faca8bb9430a24a1f85fdd12d2ea
David Betteridge <davidabetteridge@gmail.com>
2022-12-08 10:30:49 +0000

    Day8 Part 2

--- Day08/day8 part 2.py
+++ Day08/day8 part 2.py
@@ -7,7 +7,7 @@ def visible_from_tree(column_number: int, row_number: int):
   x = column_number + 1
   visible_right = 0
   while x < n_columns:
-    if int(rows[row_number][x]) == max_height:
+    if int(rows[row_number][x]) >= max_height:
       visible_right+=1
       break
     elif int(rows[row_number][x]) < max_height:
@@ -21,7 +21,7 @@ def visible_from_tree(column_number: int, row_number: int):
   x = column_number - 1
   visible_left = 0
   while x >= 0:
-    if int(rows[row_number][x]) == max_height:
+    if int(rows[row_number][x]) >= max_height:
       visible_left+=1
       break
     elif int(rows[row_number][x]) < max_height:
@@ -35,7 +35,7 @@ def visible_from_tree(column_number: int, row_number: int):
   y = row_number + 1
   visible_down = 0
   while y < n_rows:
-    if int(rows[y][column_number]) == max_height:
+    if int(rows[y][column_number]) >= max_height:
       visible_down+=1
       break
     elif int(rows[y][column_number]) < max_height:
@@ -49,8 +49,8 @@ def visible_from_tree(column_number: int, row_number: int):
   y = row_number - 1
   visible_up = 0
   while y >= 0:
-    if int(rows[y][column_number]) == max_height:
-      visible_down+=1
+    if int(rows[y][column_number]) >= max_height:
+      visible_up+=1
       break
     elif int(rows[y][column_number]) < max_height:
       y -= 1
@@ -61,7 +61,7 @@ def visible_from_tree(column_number: int, row_number: int):
   return visible_left*visible_right*visible_down*visible_up  
 
 
-with open("Day08/sample.txt") as f:
+with open("Day08/data.txt") as f:
   rows = f.read().splitlines()
   n_rows = len(rows)
   n_columns = len(rows[0])
@@ -69,11 +69,10 @@ with open("Day08/sample.txt") as f:
   seen: Set[Tuple[int,int]] = set()
   best_score = 0
 
-
   for row_number, row in enumerate(rows):
 
     for column_number in range(n_columns):
       score = visible_from_tree(column_number, row_number)
       best_score = max(best_score, score)
 
-print(best_score)      #36000 too low
+print(best_score)      #671160
