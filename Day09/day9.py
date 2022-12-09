14c033ee2b38471cc01727f87aadb8af74c55a6d
David Betteridge <davidabetteridge@gmail.com>
2022-12-09 09:24:18 +0000

    Day 9 - Part 2

--- Day09/day9.py
+++ Day09/day9.py
@@ -1,47 +1,66 @@
 
 
-from typing import Set, Tuple
+from typing import Set, Tuple, List
+
+def catch_up(head_position:Tuple[int,int], tail_position:Tuple[int,int]):
+  is_touching = abs(head_position[0] - tail_position[0]) <= 1 and \
+                abs(head_position[1] - tail_position[1]) <= 1
+
+  if not is_touching:
+    if head_position[0] == tail_position[0]:
+      # In the same column, so need to adjust the row
+      y_adjustment = 1 if head_position[1] > tail_position[1] else -1
+      tail_position = (tail_position[0], tail_position[1]+y_adjustment)
+    elif head_position[1] == tail_position[1]:
+      # In the same row, so need to adjust the column
+      x_adjustment = 1 if head_position[0] > tail_position[0] else -1
+      tail_position = (tail_position[0]+x_adjustment, tail_position[1])
+    else:
+      # We need to adjust both
+      y_adjustment = 1 if head_position[1] > tail_position[1] else -1
+      x_adjustment = 1 if head_position[0] > tail_position[0] else -1
+      tail_position = (tail_position[0]+x_adjustment, tail_position[1]+y_adjustment)
+  return tail_position
+
+def solve(rope_length: int) -> int:
+  with open("Day09/data.txt") as f:
+    
+    rope: List[Tuple[int,int]] = []
+    for _ in range(rope_length):
+      rope.append((0,0))
+    
+    visited: Set[Tuple[int,int]] = set()
+    for move in f:
+      x_offset = 0
+      y_offset = 0
+      match move.strip().split():
+        case "R", distance:
+          x_offset=1
+        case "U", distance:
+          y_offset=-1
+        case "D", distance:
+          y_offset=1
+        case "L", distance:
+          x_offset=-1
+        case _:
+          raise Exception("")
+
+      for _ in range(int(distance)):
+        # Move the head of the rope
+        rope[0] = (rope[0][0]+x_offset, rope[0][1]+y_offset)
+        
+        # Let the tails catch up
+        for i in range(rope_length-1):
+          rope[i+1] = catch_up(rope[i], rope[i+1])
+
+        visited.add(rope[-1])
+    return len(visited)
 
 
-with open("Day09/data.txt") as f:
-  tail_position = (0,0)
-  head_position = (0,0)
-  visited: Set[Tuple[int,int]] = set()
-  for move in f:
-    x_offset = 0
-    y_offset = 0
-    match move.strip().split():
-      case "R", distance:
-        x_offset=1
-      case "U", distance:
-        y_offset=-1
-      case "D", distance:
-        y_offset=1
-      case "L", distance:
-        x_offset=-1
-      case _:
-        raise Exception("")
+part1 = solve(2)
+assert part1 == 5710
 
-    for _ in range(int(distance)):
-      head_position = (head_position[0]+x_offset, head_position[1]+y_offset)
+part2 = solve(10)
+assert part2 == 2259
 
-      is_touching = abs(head_position[0] - tail_position[0]) <= 1 and \
-                    abs(head_position[1] - tail_position[1]) <= 1
-
-      if not is_touching:
-        if head_position[0] == tail_position[0]:
-          # In the same column, so need to adjust the row
-          y_adjustment = 1 if head_position[1] > tail_position[1] else -1
-          tail_position = (tail_position[0], tail_position[1]+y_adjustment)
-        elif head_position[1] == tail_position[1]:
-          # In the same row, so need to adjust the column
-          x_adjustment = 1 if head_position[0] > tail_position[0] else -1
-          tail_position = (tail_position[0]+x_adjustment, tail_position[1])
-        else:
-          # We need to adjust both
-          y_adjustment = 1 if head_position[1] > tail_position[1] else -1
-          x_adjustment = 1 if head_position[0] > tail_position[0] else -1
-          tail_position = (tail_position[0]+x_adjustment, tail_position[1]+y_adjustment)
-      visited.add(tail_position)
-
-  print(len(visited))
+print(part1, part2)
