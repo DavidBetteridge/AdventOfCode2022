6018bf904de1b5806b5d095f47b469283abf0ba2
David Betteridge <davidabetteridge@gmail.com>
2022-12-15 09:30:25 +0000

    wip

--- Day15/day15 part 2.py
+++ Day15/day15 part 2.py
@@ -29,15 +29,27 @@ with open(r"C:\Personal\AdventOfCode2022\Day15\data.txt") as f:
       Location(int(m.group("beacon_x")), int(m.group("beacon_y"))))
     sensors.append(sensor)
 
+  min_x = 0
+  min_y = 0
+  max_x = 4000000
+  max_y = 4000000
+  # tuning_frequency = (x * 4000000) + y
+  # sample_target_frequence = 56000011
 
-  target_row = 2000000
-  no_beacon_xs: Set[int] = set()
+  
+  no_beacon_xs: List[Set[int]] = []
+  for _ in range(max_y+1):
+    no_beacon_xs.append(set())
+
   for sensor in sensors:
     m_dist = distance(sensor.sensor, sensor.beacon)
-    distance_to_row = abs(sensor.sensor.y - target_row)
-    remaining = m_dist - distance_to_row
-    for x in range(sensor.sensor.x - remaining, sensor.sensor.x + remaining+1):
-      if (x != sensor.beacon.x or target_row != sensor.beacon.y): 
-        no_beacon_xs.add(x)
+    for target_row in range(max_y+1):
+      distance_to_row = abs(sensor.sensor.y - target_row)
+      remaining = m_dist - distance_to_row
+      for x in range(max(0,sensor.sensor.x - remaining), min(max_x+1, sensor.sensor.x + remaining+1)):
+        if (x != sensor.beacon.x or target_row != sensor.beacon.y): 
+          no_beacon_xs[target_row].add(x)
 
-  print(len(no_beacon_xs))   # 5073496
+  for y in range(max_y+1):
+    if len(no_beacon_xs[y]) != (max_x+1):
+      print(y,  len(no_beacon_xs[y]))
