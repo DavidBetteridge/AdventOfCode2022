e51f25856bc81b265a7e28a9f4cbb0cec8a71ae1
David Betteridge <davidabetteridge@gmail.com>
2022-12-15 20:51:05 +0000

    Day15 Part 2

--- Day15/day15 part 2.py
+++ Day15/day15 part 2.py
@@ -1,6 +1,6 @@
 import re
 from dataclasses import dataclass
-from typing import List, Set
+from typing import List, Set, Tuple
 
 
 @dataclass
@@ -37,19 +37,44 @@ with open(r"C:\Personal\AdventOfCode2022\Day15\data.txt") as f:
   # sample_target_frequence = 56000011
 
   
-  no_beacon_xs: List[Set[int]] = []
+  no_beacon_xs: List[List[Tuple[int,int]]] = []
   for _ in range(max_y+1):
-    no_beacon_xs.append(set())
+    no_beacon_xs.append([])
 
   for sensor in sensors:
     m_dist = distance(sensor.sensor, sensor.beacon)
     for target_row in range(max_y+1):
       distance_to_row = abs(sensor.sensor.y - target_row)
       remaining = m_dist - distance_to_row
-      for x in range(max(0,sensor.sensor.x - remaining), min(max_x+1, sensor.sensor.x + remaining+1)):
-        if (x != sensor.beacon.x or target_row != sensor.beacon.y): 
-          no_beacon_xs[target_row].add(x)
+      if remaining >=0:
+        no_beacon_xs[target_row].append((max(0,sensor.sensor.x - remaining),
+                                      min(max_x, sensor.sensor.x + remaining)))
+
+  def overlaps(a:Tuple[int,int],b:Tuple[int,int]) -> bool:
+    return a[0] <= (b[1]+1) and b[0] <= (a[1]+1)
+
+  def combine(a:Tuple[int,int],b:Tuple[int,int]) -> Tuple[int,int]:
+    return min(a[0],b[0]),max(a[1],b[1])
 
   for y in range(max_y+1):
-    if len(no_beacon_xs[y]) != (max_x+1):
-      print(y,  len(no_beacon_xs[y]))
+    ranges = no_beacon_xs[y]
+    try_again = True
+
+    final_ranges: List[Tuple[int,int]] = []
+    while len(ranges) > 0:
+      r = ranges.pop()
+      for i, o in enumerate(ranges):
+        if overlaps(r,o):
+          ranges.pop(i)
+          ranges.append(combine(o,r))
+          break
+      else:
+        final_ranges.append(r)
+
+    final_ranges = sorted(final_ranges)
+    if final_ranges[0][0] != 0 or final_ranges[0][1] != max_x:
+      tuning_frequency = ((final_ranges[0][1]+1)  * 4000000) + y
+      print(y, tuning_frequency)
+
+
+
