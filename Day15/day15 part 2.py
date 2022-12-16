f2e2d37313138e5e691ff8b519dd73262b9c44c5
David Betteridge <davidabetteridge@gmail.com>
2022-12-16 08:23:51 +0000

    Speed ups and timings

--- Day15/day15 part 2.py
+++ Day15/day15 part 2.py
@@ -1,7 +1,8 @@
+from collections import defaultdict
 import re
 from dataclasses import dataclass
 import time
-from typing import List, Set, Tuple
+from typing import Dict, List, Set, Tuple
 
 
 @dataclass
@@ -13,6 +14,7 @@ class Location:
 class Sensor:
   sensor: Location
   beacon: Location
+  distance: int = 0
 
 def distance(lhs: Location, rhs: Location)->int:
   return abs(lhs.x-rhs.x) + abs(lhs.y-rhs.y)
@@ -30,31 +32,28 @@ with open(r"C:\Personal\AdventOfCode2022\Day15\data.txt") as f:
     sensor = Sensor(
       Location(int(m.group("sensor_x")), int(m.group("sensor_y"))),
       Location(int(m.group("beacon_x")), int(m.group("beacon_y"))))
+    sensor.distance = distance(sensor.sensor, sensor.beacon)
     sensors.append(sensor)
 
   min_x = 0
   min_y = 0
   max_x = 4000000
   max_y = 4000000
-  # tuning_frequency = (x * 4000000) + y
-  # sample_target_frequence = 56000011
-
   
-  no_beacon_xs: List[List[Tuple[int,int]]] = []
-  for _ in range(max_y+1):
-    no_beacon_xs.append([])
+  no_beacon_xs: Dict[int, List[Tuple[int,int]]] = defaultdict(list)
+  elapsed_time = time.time() - st
+  print('PreBuild time:', elapsed_time, 'seconds')
 
   for sensor in sensors:
-    m_dist = distance(sensor.sensor, sensor.beacon)
+    m_dist = sensor.distance
     for target_row in range(max(0, sensor.sensor.y - m_dist), min(max_x, sensor.sensor.y + m_dist+1)):
-      distance_to_row = abs(sensor.sensor.y - target_row)
-      remaining = m_dist - distance_to_row
+      remaining = m_dist - abs(sensor.sensor.y - target_row)
       if remaining >=0:
-        no_beacon_xs[target_row].append((max(0,sensor.sensor.x - remaining),
-                                      min(max_x, sensor.sensor.x + remaining)))
+        lower = max(0,sensor.sensor.x - remaining)
+        upper = min(max_x, sensor.sensor.x + remaining)
+        no_beacon_xs[target_row].append((lower,upper))
   elapsed_time = time.time() - st
   print('Build time:', elapsed_time, 'seconds')
-
 
   def overlaps(a:Tuple[int,int],b:Tuple[int,int]) -> bool:
     return a[0] <= (b[1]+1) and b[0] <= (a[1]+1)
@@ -64,14 +63,16 @@ with open(r"C:\Personal\AdventOfCode2022\Day15\data.txt") as f:
 
   for y in range(max_y+1):
     ranges = no_beacon_xs[y]
-
-    for i in range(len(ranges)-1):
+    n_ranges = len(ranges)
+    for i in range(n_ranges-1):
       src = ranges[i]
-      for i2 in range(i+1, len(ranges)):
+      for i2 in range(i+1, n_ranges):
         if overlaps(src,ranges[i2]):
           ranges[i2] = combine(src,ranges[i2])
           break
       else:
+        elapsed_time = time.time() - st
+        print('Found Y:', elapsed_time, 'seconds')        
         ranges = ranges[i:]
 
         final_ranges: List[Tuple[int,int]] = []
@@ -84,11 +85,11 @@ with open(r"C:\Personal\AdventOfCode2022\Day15\data.txt") as f:
           else:
             final_ranges.append(r)
 
-        if len(final_ranges) > 1:
-          final_ranges = sorted(final_ranges)
-          if final_ranges[0][0] != 0 or final_ranges[0][1] != max_x:
-            tuning_frequency = ((final_ranges[0][1]+1)  * 4000000) + y
-            print(y, tuning_frequency)
-            elapsed_time = time.time() - st
-            print('Total time:', elapsed_time, 'seconds')
-            break
+        final_ranges = sorted(final_ranges)
+        if final_ranges[0][0] != 0 or final_ranges[0][1] != max_x:
+          tuning_frequency = ((final_ranges[0][1]+1)  * 4000000) + y
+          print(y, tuning_frequency)
+          assert tuning_frequency == 13081194638237
+          elapsed_time = time.time() - st
+          print('Total time:', elapsed_time, 'seconds')
+          break
