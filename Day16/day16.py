065b4afab4b7610ad07d48bac3d12bf091590e9b
David Betteridge <davidabetteridge@gmail.com>
2022-12-16 21:31:59 +0000

    Faster part 1

--- Day16/day16.py
+++ Day16/day16.py
@@ -1,4 +1,6 @@
+from collections import defaultdict
 import re
+import time
 import networkx as nx
 
 def remove_node(G, node: str):
@@ -14,7 +16,7 @@ def remove_node(G, node: str):
 open_valves = {}
 flow_rates = {}
 
-with open(r"C:\Personal\AdventOfCode2022\Day16\data.txt") as f:
+with open(r"C:\Personal\AdventOfCode2022\Day16\sample.txt") as f:
   # Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
   lines = f.read().splitlines()
   pattern = r"Valve (?P<valve>[A-Z]+) has flow rate=(?P<rate>[\d]+); tunnel[s]? lead[s]? to valve[s]? "
@@ -44,21 +46,21 @@ with open(r"C:\Personal\AdventOfCode2022\Day16\data.txt") as f:
   for node in to_remove:
     remove_node(G, node)
 
+  out_edges = defaultdict(list)
+  for (from_,to_,details) in G.edges(data=True):
+    out_edges[from_].append((to_, details["distance"]))
+
 
   def solve(G, from_location, location, moves_renamining, total_flow) -> int:
     if moves_renamining <= 1:
       return total_flow
 
-    # Switch on / yes
-    # Switch on,  update graph,  increase total_flow,  decrease moves remaining
-
     # Move from this room without switching on any taps
     best_total_flow = total_flow
-    out_edges = list(G.out_edges(location,data=True))
-    for _,out_edge,data in out_edges:
+    for out_edge,distance in out_edges[location]:
       if out_edge != from_location:
-        if data["distance"] < moves_renamining:
-          best_total_flow = max(best_total_flow, solve(G, location, out_edge, moves_renamining-data["distance"], total_flow))
+        if distance < moves_renamining:
+          best_total_flow = max(best_total_flow, solve(G, location, out_edge, moves_renamining-distance, total_flow))
 
     is_open = open_valves[location]
     flow = flow_rates[location]
@@ -67,18 +69,17 @@ with open(r"C:\Personal\AdventOfCode2022\Day16\data.txt") as f:
       total_flow += moves_renamining * flow
       best_total_flow = max(best_total_flow,total_flow)
       open_valves[location]=True
-      for _,out_edge,data in out_edges:
-        if data["distance"] < moves_renamining:
-          best_total_flow = max(best_total_flow, solve(G, location, out_edge, moves_renamining-data["distance"], total_flow))
+      for out_edge,distance in out_edges[location]:
+        if distance < moves_renamining:
+          best_total_flow = max(best_total_flow, solve(G, location, out_edge, moves_renamining-distance, total_flow))
       open_valves[location]=False
 
 
     return best_total_flow
 
-
-  print(solve(G, "", "AA", 30, 0))  #1651
-
-    
-
+  st = time.time()
+  assert solve(G, "", "AA", 30, 0) == 1651 #2330 # 1651
+  elapsed_time = time.time() - st
+  print('Total time:', elapsed_time, 'seconds')
 
 
