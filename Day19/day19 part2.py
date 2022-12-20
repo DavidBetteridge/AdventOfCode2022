2864f738926a59410f6c6e21a370b89a2cc50e4b
David Betteridge <davidabetteridge@gmail.com>
2022-12-20 23:19:51 +0000

    Day 19 Part 2

--- Day19/day19 part2.py
+++ Day19/day19 part2.py
@@ -38,33 +38,77 @@ CLAY=1
 OBSIDIAN=2
 GEODE=3
 
+tri = [0,1,3,6,10,15,21,28,36,45,55,61,73]
+
 def solve_blueprint(all_requirements: Tuple[Tuple[int,int,int],...],
                     time_allowed: int):
   initial_stock = (0,0,0,0)
   built_robots = (1,0,0,0)
-  MAX_ORE_REQUIRED = max(q[ORE] for q in all_requirements[:-1])
-  MAX_CLAY_REQUIRED = max(q[CLAY] for q in all_requirements[:-1])  
-  
+  best = 0
+
+  def estimate(time: int,
+               stock: Tuple[int,int,int,int],
+               robots: Tuple[int,int,int,int]):
+    used_ore_for_ore = 0
+    used_ore_for_clay = 0
+    used_ore_for_obs = 0
+    used_clay_for_obs = 0
+    used_ore_for_geo = 0
+    used_obs_for_geo = 0
+    for _ in range(time, time_allowed+1):
+      if all_requirements[ORE][ORE] <= (stock[ORE] - used_ore_for_ore):
+        robots = (robots[ORE]+1,robots[CLAY],robots[OBSIDIAN],robots[GEODE])
+        used_ore_for_ore+=all_requirements[ORE][ORE]
+
+      if all_requirements[CLAY][ORE] <= (stock[ORE]- used_ore_for_clay):
+        robots = (robots[ORE],robots[CLAY]+1,robots[OBSIDIAN],robots[GEODE])
+        used_ore_for_clay+=all_requirements[CLAY][ORE]
+
+      if (all_requirements[OBSIDIAN][ORE] <= (stock[ORE]-used_ore_for_obs)) and \
+         (all_requirements[OBSIDIAN][CLAY] <= (stock[CLAY]-used_clay_for_obs)):
+        robots = (robots[ORE],robots[CLAY],robots[OBSIDIAN]+1,robots[GEODE])
+        used_ore_for_obs+=all_requirements[OBSIDIAN][ORE]
+        used_clay_for_obs+=all_requirements[OBSIDIAN][CLAY]
+
+      if (all_requirements[GEODE][ORE] <= (stock[ORE]-used_ore_for_geo)) and \
+         (all_requirements[GEODE][OBSIDIAN] <= (stock[OBSIDIAN]-used_obs_for_geo)):
+        robots = (robots[ORE],robots[CLAY],robots[OBSIDIAN],robots[GEODE]+1)
+        used_ore_for_geo+=all_requirements[GEODE][ORE]
+        used_obs_for_geo+=all_requirements[GEODE][OBSIDIAN]
+
+      stock = (stock[0] + robots[0],
+                  stock[1] + robots[1],
+                  stock[2] + robots[2],
+                  stock[3] + robots[3])
+    return stock[GEODE]
+
   @lru_cache(maxsize=None)
   def solve(time: int,
             stock: Tuple[int,int,int,int],
             robots: Tuple[int,int,int,int]):
+    nonlocal best
     time += 1
     if time > time_allowed:
       return stock[GEODE]
+
+    if best > estimate(time,stock,robots):
+      return best
 
     # Produce new stock
     new_stock = (stock[0] + robots[0],
                 stock[1] + robots[1],
                 stock[2] + robots[2],
                 stock[3] + robots[3])
-    
-    best = new_stock[GEODE]
+
+    best = max(best, new_stock[GEODE])
     if time < time_allowed:
       # Always build Geode if we can
       requirements = all_requirements[GEODE]
       if stock[ORE] >= requirements[ORE] and \
         stock[OBSIDIAN] >= requirements[OBSIDIAN]:
+        if time == (time_allowed-1):
+          return 1 + new_stock[GEODE] + robots[GEODE]
+
         new_robots = (robots[0],robots[1],robots[2],robots[3]+1)
         reduced_stock = (new_stock[ORE] - requirements[ORE],
                           new_stock[1],
@@ -73,6 +117,9 @@ def solve_blueprint(all_requirements: Tuple[Tuple[int,int,int],...],
         best = max(best,
                   solve(time,reduced_stock, new_robots))
       else:
+        if time == (time_allowed-1):
+          return new_stock[GEODE] + robots[GEODE]
+
         done_something = False
         can_create_ore = stock[ORE] >= all_requirements[ORE][ORE]
         can_create_clay = stock[ORE] >= all_requirements[CLAY][ORE] 
@@ -110,18 +157,28 @@ def solve_blueprint(all_requirements: Tuple[Tuple[int,int,int],...],
           best = max(best, solve(time, new_stock, robots))
 
     return best
-  return solve(0, initial_stock, built_robots)
-
+  solve(0, initial_stock, built_robots)
+  return best
 
 with open(r"C:\Personal\AdventOfCode2022\Day19\data.txt") as f:
   lines = f.read().splitlines()
 
-  quality=0
-  for line in lines:
-    bp_number, requirements = parse_line(line)
-    best = solve_blueprint(requirements, time_allowed=24)
-    print(bp_number, best)
-    quality += (bp_number * best)
-  print(quality)
-  assert quality == 62
+# Part 1
+quality=0
+for line in lines:
+  bp_number, requirements = parse_line(line)
+  best = solve_blueprint(requirements, time_allowed=24)
+  print(bp_number, best)
+  quality += (bp_number * best)
+print(quality)
+assert quality == 1725
 
+# Part 2
+total=1
+for line in lines[:3]:
+  bp_number, requirements = parse_line(line)
+  best = solve_blueprint(requirements, time_allowed=32)
+  print(bp_number, best)
+  total *= best
+print(total)
+assert total == 15510
