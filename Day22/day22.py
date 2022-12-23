01050e97d70ecec02a2a139d821a2820212d105b
David Betteridge <davidabetteridge@gmail.com>
2022-12-23 00:39:23 +0000

    Part 2

--- Day22/day22.py
+++ Day22/day22.py
@@ -27,16 +27,16 @@ with open("Day22/data.txt") as f:
 
   # Label all the verticies on each face,  from the top-left -> bottom-right
   vertices = {}
-  edges = set()
+  edges = dict()
   for row_number in range(0, n_rows, side_length):
     for column_number in range(0, n_columns, side_length):
       if column_number < len(rows[row_number]) and rows[row_number][column_number] != ' ':
         labels = [ascii_lowercase[len(vertices)+i] for i in range(4)]
-        edges.add((labels[0],labels[1],"U"))
-        edges.add((labels[1],labels[3],"R"))
-        edges.add((labels[0],labels[2],"L"))
-        edges.add((labels[2],labels[3],"D"))
-        for col_off, row_off, label_index in [(0,0,0),(49,0,1),(0,49,2),(49,49,3)]:
+        edges[(labels[0],labels[1])]="U"
+        edges[(labels[1],labels[3])]="R"
+        edges[(labels[0],labels[2])]="L"
+        edges[(labels[2],labels[3])]="D"
+        for col_off, row_off, label_index in [(0,0,0),(side_length-1,0,1),(0,side_length-1,2),(side_length-1,side_length-1,3)]:
           vertices[labels[label_index]] = ((column_number+col_off, row_number+row_off))
 
   # Ideally we would find their pairs by folding the net around a centre point
@@ -57,13 +57,30 @@ with open("Day22/data.txt") as f:
     ("m","o"): ("c","a"),
   }
 
+  # pairs: Dict[Tuple[str,str],Tuple[str,str]] = {
+  #   ("a","b"): ("f","e"),
+  #   ("a","c"): ("i","j"),
+  #   ("b","d"): ("x","v"),
+  #   ("n","p"): ("v","u"),
+  #   ("k","l"): ("q","s"),
+  #   ("g","h"): ("t","s"),
+  #   ("e","g"): ("x","w"),
+  #   ("e","f"):("b","a"),
+  #   ("i","j"):("a","c"),
+  #   ("v","x"):("d","b"),
+  #   ("u","v"):("p","n"),
+  #   ("q","s"):("k","l"),
+  #   ("s","t"):("h","g"),
+  #   ("w","x"):("g","e"),
+  # }
+
   def find_edge(x,y, direction) -> Tuple[str,str]:
-    for v1,v2,dir in edges:
-      if dir == direction:
+    for v1,v2 in edges:
+      if edges[(v1,v2)] == direction:
         x1,y1 = vertices[v1]
         x2,y2 = vertices[v2]
 
-        if dir in ("U", "D"):
+        if direction in ("U", "D"):
           if (y == y1) and ((x1 <= x <= x2) or (x2 <= x <= x1)):
             return v1,v2
         else:
@@ -111,7 +128,23 @@ with open("Day22/data.txt") as f:
     edge = find_edge(x,y, direction)
     offset = find_offset(edge, x,y)
     opposite_edge = pairs[edge]
-    return apply_offset(opposite_edge, offset)
+    
+    a,b = opposite_edge
+    if a > b:
+      next_direction = edges[(b,a)]
+    else:
+      next_direction = edges[(a,b)]
+
+    if next_direction == "U":
+      next_direction = "D"
+    elif next_direction == "D":
+      next_direction = "U"
+    elif next_direction == "L":
+      next_direction = "R"
+    elif next_direction == "R":
+      next_direction = "L"
+
+    return *apply_offset(opposite_edge, offset),next_direction
 
   current_location = None
   current_direction = "R"
@@ -138,37 +171,41 @@ with open("Day22/data.txt") as f:
 
           if direction == "R":
             if (column_number+1) > row_end:
-              next_column_number,next_row_number = switch_side(column_number,row_number, direction)
+              next_column_number,next_row_number,next_direction = switch_side(column_number,row_number, direction)
             else:
               next_row_number = row_number
               next_column_number = column_number + 1
+              next_direction = direction
 
           elif direction == "L":
             if (column_number-1) < row_start:
-              next_column_number,next_row_number = switch_side(column_number,row_number, direction)
+              next_column_number,next_row_number,next_direction = switch_side(column_number,row_number, direction)
             else:
               next_row_number = row_number
               next_column_number = column_number - 1
+              next_direction = direction
 
           elif direction == "U":
             if (row_number-1) < column_start:
-              next_column_number,next_row_number = switch_side(column_number,row_number, direction)
+              next_column_number,next_row_number,next_direction = switch_side(column_number,row_number, direction)
             else:
               next_column_number = column_number
               next_row_number = row_number - 1
+              next_direction = direction
 
           elif direction == "D":
             if (row_number+1) > column_end:
-              next_column_number,next_row_number = switch_side(column_number,row_number, direction)
+              next_column_number,next_row_number,next_direction = switch_side(column_number,row_number, direction)
             else:
               next_column_number = column_number
               next_row_number = row_number + 1
+              next_direction = direction
 
           else:
             raise Exception("Unknown direction")
 
           if rows[next_row_number][next_column_number] == ".":
-            G.add_edge((column_number, row_number),(next_column_number,next_row_number),direction=direction)
+            G.add_edge((column_number, row_number),(next_column_number,next_row_number),direction=direction,next_direction=next_direction)
   moves = {
     "R": (1,0),
     "L": (-1,0),
@@ -202,8 +239,8 @@ with open("Day22/data.txt") as f:
     "U" : 3
   }
 
-  #10R5L5R10L4R5L5
   i = 0
+  debug = {}
   while i < len(commands):
     command = ""
     while i < len(commands) and commands[i].isdigit():
@@ -217,12 +254,15 @@ with open("Day22/data.txt") as f:
                        if edge[2]["direction"]==current_direction]
         if len(valid_moves)>0:
           current_location = valid_moves[0][1]
+          current_direction = valid_moves[0][2]["next_direction"]
+          debug[current_location] = current_direction
         else:
           break
       
     else:
       # We have a direction
       current_direction = turns[commands[i]][current_direction]
+      debug[current_location] = current_direction
       i+=1
     # print(current_location, current_direction)
 
@@ -232,3 +272,16 @@ with open("Day22/data.txt") as f:
 
   #101048 toolow
   # not 147040
+  #10R5L5R10L4R5L5
+
+  for row_number in range(0, n_rows):
+    for column_number in range(0, n_columns):
+      cd = debug.get((column_number,row_number),"")
+      if cd == "":
+        if column_number < len(rows[row_number]):
+          print(rows[row_number][column_number],end="")
+        else:
+          print(" ",end="")
+      else:
+        print(cd,end="")
+    print("")
